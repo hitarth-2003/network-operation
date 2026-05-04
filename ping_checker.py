eb03311f648e7fb6edee792259ca9d04aba057ac
hitarth-2003 <hitarthchoubisa9680@gmail.com>
2026-05-04 23:46:42 +0530

    new commit

--- ping_checker.py
+++ ping_checker.py
@@ -8,4 +8,11 @@ def ping_function():
     else:
         print(f"response time : {response*1000:.2f} ms")
 
+while True:
+    print("1. Ping")
+    print("2. Traceroute")
+    choice = input("Choose option: ")
+
+
+
 ping_function()
